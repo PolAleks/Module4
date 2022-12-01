407469c0bd28e5ce348a1721b4c78232c6c9477c
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-01 10:28:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Module4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "test1";
+            string B = "test2";
+            bool C = A == B;
+            Console.WriteLine(C);
+        }
+    }
+}
