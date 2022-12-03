5e70157cb5ea9c0607c155f0e990a1b530134b13
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-03 14:07:53 +0300

    Complite the task 4.3.2

--- Program.cs
+++ Program.cs
@@ -4,38 +4,52 @@
     {
         static void Main(string[] args)
         {
-            int t = 0;
-            do
+            var arr = new int[] {2,6,3,-3,6,9,10,56,3,2,1 };
+            int minValue = int.MaxValue,
+                minIndex = -1;
+
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(t++);
-
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                var color = Console.ReadLine();
-
-                switch (color)
+                if (arr[i] < minValue)
                 {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Your color is {0}", color);
-                        break;
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Your color is {0}", color);
-                        break;
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Your color is {0}", color);
-                        break;
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Your color is yellow");
-                        break;
+                    minValue = arr[i];
+                    minIndex = i;
                 }
-            } while(t < 3);
+            }
+            Console.WriteLine("Наименьший элемент массива {0}, зщначение этого элемента = {1}", minIndex, minValue);
+
+            //int t = 0;
+            //do
+            //{
+            //    Console.WriteLine(t++);
+
+            //    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            //    var color = Console.ReadLine();
+
+            //    switch (color)
+            //    {
+            //        case "red":
+            //            Console.BackgroundColor = ConsoleColor.Red;
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.WriteLine("Your color is {0}", color);
+            //            break;
+            //        case "green":
+            //            Console.BackgroundColor = ConsoleColor.Green;
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.WriteLine("Your color is {0}", color);
+            //            break;
+            //        case "cyan":
+            //            Console.BackgroundColor = ConsoleColor.Cyan;
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.WriteLine("Your color is {0}", color);
+            //            break;
+            //        default:
+            //            Console.BackgroundColor = ConsoleColor.Yellow;
+            //            Console.ForegroundColor = ConsoleColor.Red;
+            //            Console.WriteLine("Your color is yellow");
+            //            break;
+            //    }
+            //} while(t < 3);
         }
     }
 }
