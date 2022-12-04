5d6571290224378155f06f418c51f8d27ad7b0e9
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-04 11:53:46 +0300

    Complite the task 4.3.16

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, -3, 4, -2, -6, 8, 9, 10, 11 };
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
             int sumPositive = 0;
-            foreach(int i in array) 
+            foreach(int i in arr) 
                 if (i >= 0) sumPositive++;
 
             Console.WriteLine("Количество положительных чисел: {0}", sumPositive);                        
