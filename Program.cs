174ee3ceb099172c03488cd4e5423bd8a62b458e
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-04 17:11:02 +0300

    4.5.5 - Вводим возраст и любимые цвета пользователя

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             // Анкетирование пользователя
             
-            (string firstName, string lastName, string login, int loginLenght, bool hasPet, int age, string[] favColors) User;
+            (string firstName, string lastName, string login, int loginLenght, bool hasPet, double age, string[] favColors) User;
             
             Console.WriteLine("Введите имя: ");
             User.firstName = Console.ReadLine();
@@ -22,7 +22,17 @@
                 User.hasPet = true;
             else
                 User.hasPet = false;
-            
+
+            Console.WriteLine("Введите возраст пользователя: ");
+            User.age = double.Parse(Console.ReadLine());
+
+            User.favColors = new string [3];
+            Console.WriteLine("Введите три любимых цвета пользователя");
+            for(int i = 0; i < User.favColors.Length; i++) 
+            {
+                Console.Write("{0} любимый цвет: ", i + 1);
+                User.favColors[i] = Console.ReadLine() + "\n";
+            }
         }
     }
 }
