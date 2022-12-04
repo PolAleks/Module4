bec8f790864368fa4bbf25be845b47d11d7830d8
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-04 10:33:57 +0300

    Complite the task 4.3.11

--- Program.cs
+++ Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 } };
-            foreach(int i in array)
+            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++) 
             {
-                Console.Write(i + " ");
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                    Console.Write(array[k, i] + " ");
+                Console.WriteLine();
             }
+            
 
             //int t = 0;
             //do
